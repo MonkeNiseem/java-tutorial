e66004207b3cb2fcb95d07f8ee72bcbd0aa142ec
Niseem Bhattacharya <96676377+MonkeNiseem@users.noreply.github.com>
2025-03-18 13:25:08 -0500

    Update ShoppingCart.java

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,1 +1,35 @@
+import java.util.Scanner;
 
+public class ShoppingCart{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String itemName;
+        double itemPrice;
+        int itemQuantity;
+        
+        System.out.print("What is the item you are buying?: ");
+        itemName = scanner.nextLine();
+        
+        System.out.print("What is the price of each item?: ");
+        itemPrice = scanner.nextDouble();
+        
+        System.out.print("How much of the item are you buying?: ");
+        itemQuantity = scanner.nextInt();
+        
+        double totalItemPrice = itemPrice*itemQuantity;
+        
+        
+        if (itemQuantity == 1){
+            System.out.println("\nYou have bought 1 " + itemName);
+        }
+        else{
+            System.out.println("\nYou have bought " + itemQuantity + " " + itemName + "s");
+        }
+        
+        System.out.println("Your total is $" + totalItemPrice);
+        
+        
+        scanner.close();
+    }
+}
