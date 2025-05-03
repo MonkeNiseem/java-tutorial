79166f673a7878e39660751330018f05878e9641
Niseem <niseem2008@gmail.com>
2025-05-02 20:29:55 -0500

    coming back to this tutorial after 2 month break, getting refreshed with everything

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,5 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter your email: ");
-        String email = scanner.nextLine();
-
-        if(email.contains("@")){
-            String username = email.substring(0, email.indexOf("@"));
-            String domain = email.substring(email.indexOf("@")+1);
-
-            System.out.println(username);
-            System.out.println(domain);
-        }
-        else{
-            System.out.println("Invalid email!");
-        }
-
-        scanner.close();
-
     }
 }
