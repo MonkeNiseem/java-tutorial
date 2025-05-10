0fd9363627f81de280e5c2868466ff8c9801c80c
Niseem Bhattacharya <96676377+MonkeNiseem@users.noreply.github.com>
2025-05-10 17:38:18 -0500

    Create tempconverter.java

---
+++ src/tempconverter.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class tempconverter
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter a temperature: ");
+		double temp = scanner.nextDouble();
+		scanner.nextLine();
+		
+        System.out.print("Convert to Celsius or Fahrenheit? (C or F): ");
+        String conversion = scanner.nextLine();
+        
+        if (conversion.equalsIgnoreCase("c")){
+            temp = (temp * (9.0/5.0)) + 32;
+            System.out.printf("%.1f°F", temp);
+        }
+        else if (conversion.equalsIgnoreCase("f")){
+            temp = (temp - 32) * (5.0/9.0);
+            System.out.printf("%.1f°C", temp);
+        }
+        else {
+            System.out.print("Invalid response!");
+        }
+        
+        scanner.close();
+	}
+}
